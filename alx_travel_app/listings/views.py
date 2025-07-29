aff00e8b709e02690a1b109be82edf7d6767d7e0
Demarkus <demarkusmcnabb@gmail.com>
2025-07-29 02:58:19 -0700

    API Development for Listings and Bookings in Django

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render # type: ignore
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing Listings.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing Bookings.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all bookings
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Booking.objects.filter(user=user)
