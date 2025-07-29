413cfc8d0a0856e0a93d9f8c7188e9ad3ccc2036
Demarkus <demarkusmcnabb@gmail.com>
2025-07-29 03:12:11 -0700

    API Development for Listings and Bookings in Django

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render # type: ignore
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing listings.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing bookings.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -33,5 +33,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # ... other urls
+    path('api/', include('listings.urls')),
 ]
